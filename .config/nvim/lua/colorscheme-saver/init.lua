3eda7f5d16751930c5cfd24e94f178ba0a95a5b5
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-11-10 16:27:59 +0000

    re-write colorscheme-saver plugin to handle state and IO more robustly

--- .config/nvim/lua/colorscheme-saver/init.lua
+++ .config/nvim/lua/colorscheme-saver/init.lua
@@ -3,57 +3,42 @@
 
 local M = {}
 
-local function ensure_file_exists(filename)
-	local file = io.open(filename, "r") -- Try to open in read mode
-	if file then
-		-- File exists, close it
-		file:close()
-	else
-		-- File does not exist, create it
-		file = io.open(filename, "w") -- Open in write mode to create (or truncate)
-		if file then
-			file:close()
-			print("File '" .. filename .. "' created successfully.")
-		else
-			print("Error: Could not create file '" .. filename .. "'.")
-		end
-	end
-end
+local homedir = os.getenv("HOME")
+local state_dir = homedir .. "/.local/state/nvim-colorscheme-saver"
+local colorscheme_filename = "current_theme.txt"
+local colorscheme_filepath = state_dir .. "/" .. colorscheme_filename
 
-local function set_colorscheme(filename)
-	local file = io.open(filename, "r")
+local function get_file_for_writing(filepath)
+	local file, err = io.open(filepath, "w")
 	if not file then
-		print("Error: Could not open file '" .. filename .. "' for reading.")
-		return
+		error("unable to open or create file at " .. filepath .. ": " .. err)
 	end
-
-	local scheme = file:read("*l") -- read first line
-	file:close()
-
-	if not scheme or scheme == "" then
-		-- we'll just use default until user sets one
-		return
-	end
-
-	local ok, err = pcall(vim.cmd, "colorscheme " .. scheme)
-	if not ok then
-		print("Error: Could not set colorscheme '" .. scheme .. "': " .. err)
-	end
+	return file
 end
-
-local colorscheme_filepath = os.getenv("HOME") .. "/.config/nvim/lua/custom/colorscheme-saver/current_scheme.txt"
 
 M.setup = function()
-	set_colorscheme(colorscheme_filepath) -- Help needed here
+	vim.fn.mkdir(state_dir, "p")
+	local file = get_file_for_writing(colorscheme_filepath)
+
+	local scheme = file:read("*l")
+	file:close()
+	if scheme and scheme ~= "" then
+		local ok, err = pcall(vim.cmd, "colorscheme" .. scheme)
+		if not ok then
+			print("error setting colorscheme " .. scheme .. ": " .. err)
+		end
+	end
+
 	vim.api.nvim_create_autocmd("ColorScheme", {
 		callback = function(args)
-			ensure_file_exists(colorscheme_filepath)
-			vim.fn.jobstart({ "sh", "-c", "echo '" .. args.match .. "' > " .. colorscheme_filepath })
-
-			-- Maybe at some point we should also sync alacritty
-			-- theme with this... but I have to tackle the alacritty
-			-- side of this too.
-			-- vim.fn.jobstart("bash -c 'source location_of_functions_file && set_alactritty_color " .. args.match .. "'")
+			file = get_file_for_writing(colorscheme_filepath)
+			local ok, err = pcall(function(c)
+				file:write(c)
+			end, args.match)
+			file:close()
+			if not ok then
+				error("error writing coloscheme to file " .. err)
+			end
 		end,
 	})
 end
